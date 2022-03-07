0b4a8acb02b5d97d807e625cb2d581a12597a2b7
Lucas Otoni <lotoni.vivo@gmail.com>
2022-03-07 19:11:19 -0300

    PROGRAMA 4

--- Fundamentos/Bloco-4-introdução-javascript/dia-1-javascript-primeiros-passos/script.js
+++ Fundamentos/Bloco-4-introdução-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -43,3 +43,15 @@
 //     console.log(c)
 // }
 
+//PROGRAMA 4
+
+// const a = 0;
+
+// if (a > 0){
+//     console.log("positive")
+// } else if (a < 0){
+//     console.log("negative")
+// } else{
+//     console.log("Zero")
+// }
+
