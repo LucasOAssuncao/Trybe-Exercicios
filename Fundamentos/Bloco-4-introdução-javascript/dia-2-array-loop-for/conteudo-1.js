aa86242da1f03a314a4bb6107039390e2667873a
Lucas Otoni <lotoni.vivo@gmail.com>
2022-03-08 15:16:40 -0300

    Conteudo 1- atividades

---
+++ Fundamentos/Bloco-4-introdução-javascript/dia-2-array-loop-for/conteudo-1.js
@@ -0,0 +1,20 @@
+//EXERCICIO 1
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+//EXERCICIO 2
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+//EXERCICIO 3
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push("Contato");
+
+// console.log(menu);
