e6db84c910b4a8da53d888de43d9536d914313ff
Lucas Otoni <lotoni.vivo@gmail.com>
2022-03-08 19:05:47 -0300

    atividade 9

--- Fundamentos/Bloco-4-introdução-javascript/dia-2-array-loop-for/atividade-1.js
+++ Fundamentos/Bloco-4-introdução-javascript/dia-2-array-loop-for/atividade-1.js
@@ -72,11 +72,29 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //EXERCICIO 7
 
-let menorNumero = numbers[0];
+// let menorNumero = numbers[0];
 
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] < menorNumero){
-    menorNumero = numbers[i];
-  }
-}
-console.log(menorNumero);
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] < menorNumero){
+//     menorNumero = numbers[i];
+//   }
+// }
+// console.log(menorNumero);
+
+//EXERCICIO 8
+
+// let numeros = [];
+
+// for (let i = 0; i <= 25; i++) { 
+//   numeros.push(i);
+// }
+
+// console.log(numeros);
+
+//EXERCICIO 9
+
+// let numeros = [1,  2,  3,  4,  5,  6,  7, 8,  9, 10, 11, 12, 13, 14, 15,16, 17, 18, 19, 20, 21, 22, 23,24, 25];
+
+// for (i = 0; i < numeros.length; i++){
+//   console.log('A divisão de ' + numeros[i] + ' por 2 é = a: ' + numeros[i] / 2)
+// }
