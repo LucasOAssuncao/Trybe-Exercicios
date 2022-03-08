c5cf336cdc91ee821fbc824f7f584985455b3964
Lucas Otoni <lotoni.vivo@gmail.com>
2022-03-08 18:28:05 -0300

    atividade 4

--- Fundamentos/Bloco-4-introdução-javascript/dia-2-array-loop-for/atividade-1.js
+++ Fundamentos/Bloco-4-introdução-javascript/dia-2-array-loop-for/atividade-1.js
@@ -12,9 +12,32 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // for (let i = 0; i < numbers.length; i++){
 //     soma += numbers[i];
-    
+
 // }
 //   console.log(soma);
 
-//EXERCICIO 2
+//EXERCICIO 3
 
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i++){
+//     soma += numbers[i];
+
+// }
+//   console.log(soma / numbers.length);
+
+//EXERCICIO 4
+
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   soma += numbers[i];
+// }
+
+// let media = soma / numbers.length;
+
+// if (media > 20) {
+//   console.log("valor maior que 20");
+// } else {
+//   console.log("valor menor que 20");
+// }
