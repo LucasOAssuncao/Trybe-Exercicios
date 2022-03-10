a3dce66930fa3c5f1fef82e08f0fd2f1b6c15cb0
Lucas Otoni <lotoni.vivo@gmail.com>
2022-03-10 17:51:40 -0300

    atividade do conteudo

---
+++ Fundamentos/Bloco-4-introdução-javascript/dia-4-javascript-objetos-e-funcoes/conteudo.js
@@ -0,0 +1,119 @@
+// // let diasDaSemana = {
+// //     1: 'domingo',
+// //     2: 'segunda',
+// //     3: 'terça',
+// //     4: 'quarta',
+// //     5: 'quinta',
+// //     6: 'sexta',
+// //     7: 'sábado',
+// //     };
+    
+// //     diasDaSemana['1']; // SyntaxError: Unexpected number
+// //     console.log(diasDaSemana['1']); // domingo
+
+// // let conta = {
+// //     agencia: '0000',
+// //     banco: {
+// //       cod: '012',
+// //       id: 4,
+// //       nome: 'TrybeBank',
+// //     },
+// //   };
+  
+// //   let infoDoBanco = 'banco';
+// //   console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+// //   console.log(conta[infoDoBanco]['nome']); // TrybeBank
+  
+// //   console.log(conta.agencia); // 0000
+// //   console.log(conta['agencia']); // 0000
+  
+// //   console.log(conta.banco.cod); // 012
+// //   console.log(conta['banco']['id']); // 4
+
+// // let usuario = {
+// //     id: 99,
+// //     email: 'jakeperalta@gmail.com',
+// //     infoPessoal: {
+// //       nome: 'Jake',
+// //       sobrenome: 'Peralta',
+// //       endereco: {
+// //         rua: 'Smith Street',
+// //         bairro: 'Brooklyn',
+// //         cidade: 'Nova Iorque',
+// //         estado: 'Nova Iorque',
+// //       },
+// //     },
+// //   };
+  
+// //   console.log(usuario.id); // 99
+// //   console.log(usuario.email); // jakeperalta@gmail.com
+  
+// //   console.log(usuario.infoPessoal.endereco['rua']); // Smith Street
+// //   console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
+
+// // let moradores = [
+// //     {
+// //       nome: 'Luiza',
+// //       sobrenome: 'Guimarães',
+// //       andar: 10,
+// //       apartamento: 1005,
+// //     },
+// //     {
+// //       nome: 'William',
+// //       sobrenome: 'Albuquerque',
+// //       andar: 5,
+// //       apartamento: 502,
+// //     },
+// //     {
+// //       nome: 'Murilo',
+// //       sobrenome: 'Ferraz',
+// //       andar: 8,
+// //       apartamento: 804,
+// //     },
+// //     {
+// //       nome: 'Zoey',
+// //       sobrenome: 'Brooks',
+// //       andar: 1,
+// //       apartamento: 101,
+// //     },
+// //   ];
+  
+// //   let primeiroMorador = moradores[0];
+// //   console.log(primeiroMorador); // { nome: 'Luiza', sobrenome: 'Guimarães', andar: 10, apartamento: 1005 }
+// //   console.log(primeiroMorador['andar']); // 10
+  
+// //   let ultimoMorador = moradores[moradores.length - 1];
+// //   console.log(ultimoMorador); // { nome: 'Zoey', sobrenome: 'Brooks', andar: 1, apartamento: 101 }
+// //   console.log(ultimoMorador.nome); // Zoey
+
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: { golden: 2, silver: 3 },
+//     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+// };
+
+// console.log("a jogadora " + player['name'] + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.')
+// console.log("a jogadora " + player['name'] + ' ' + player.lastName + ' foi eleita a melhor do mundo por 6 vezes nos anos:' + player.bestInTheWorld[1] )
+// console.log('possui ' + player['medals']['golden'] + ' medalhas de ouro e ' + player['medals']['silver'] + ' de prata')
+
+// // let names = {
+// //     person1: 'João',
+// //     person2: 'Maria',
+// //     person3: 'Jorge',
+// //   };
+
+// //   for ( let i in names){
+// //       console.log('ola ' + names[i]);
+// //   };
+
+// //   let car = {
+// //     model: 'A3 Sedan',
+// //     manufacturer: 'Audi',
+// //     year: 2020
+// //   };
+
+// //   for (let i in car){
+// //       console.log(i, car[i]);
+// //   };
