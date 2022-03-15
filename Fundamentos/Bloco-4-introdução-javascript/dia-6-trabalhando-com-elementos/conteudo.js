c14e56f4e27286451db54a7f0e1f227f6beccbd3
Lucas Otoni <lotoni.vivo@gmail.com>
2022-03-15 15:39:33 -0300

    conteudo (segunda parte)

--- Fundamentos/Bloco-4-introdução-javascript/dia-6-trabalhando-com-elementos/conteudo.js
+++ Fundamentos/Bloco-4-introdução-javascript/dia-6-trabalhando-com-elementos/conteudo.js
@@ -10,4 +10,30 @@ document.querySelector("#elementoOndeVoceEsta").nextSibling
 
 document.querySelector("#elementoOndeVoceEsta").nextElementSibling
 
-console.log(document.querySelector("#pai").firstElementChild.nextElementSibling.nextElementSibling)
+document.querySelector("#pai").firstElementChild.nextElementSibling.nextElementSibling
+
+//Crie um irmão para elementoOndeVoceEsta .
+let irmao = document.createElement('section');
+irmao.innerText = 'abcd';
+document.querySelector("#pai").appendChild(irmao)
+
+
+// Crie um filho para elementoOndeVoceEsta .
+
+let filho = document.createElement('section');
+filho.innerText = 'lul';
+document.querySelector("#elementoOndeVoceEsta").appendChild(filho);
+console.log(document.querySelector("#elementoOndeVoceEsta").children)
+
+// Crie um filho para primeiroFilhoDoFilho .
+
+let filhoDois = document.createElement('section');
+filhoDois.innerText = 'lol';
+document.querySelector("#primeiroFilhoDoFilho").appendChild(filhoDois);
+console.log(document.querySelector("#primeiroFilhoDoFilho").children)
+
+// A partir desse filho criado, acesse terceiroFilho .
+
+let terceiroFilho = filhoDois.parentElement.parentElement.nextElementSibling
+
+console.log(terceiroFilho);
