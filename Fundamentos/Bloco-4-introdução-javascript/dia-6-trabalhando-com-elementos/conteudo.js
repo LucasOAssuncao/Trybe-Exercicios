e778331d679190fb7cb48fa197f183e9ad8895f1
Lucas Otoni <lotoni.vivo@gmail.com>
2022-03-15 15:03:22 -0300

    conteudo do dia(começo)

---
+++ Fundamentos/Bloco-4-introdução-javascript/dia-6-trabalhando-com-elementos/conteudo.js
@@ -0,0 +1,13 @@
+document.querySelector("#elementoOndeVoceEsta").parentNode.style.color = 'blue';
+
+document.querySelector("#primeiroFilhoDoFilho").textContent = 'um dois tres';
+
+document.querySelector("#pai").firstChild;
+
+document.querySelector("#elementoOndeVoceEsta").previousElementSiblinge;
+
+document.querySelector("#elementoOndeVoceEsta").nextSibling
+
+document.querySelector("#elementoOndeVoceEsta").nextElementSibling
+
+console.log(document.querySelector("#pai").firstElementChild.nextElementSibling.nextElementSibling)
