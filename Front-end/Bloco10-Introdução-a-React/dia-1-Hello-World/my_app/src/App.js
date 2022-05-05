b6be796a17971f307c2391caad233c41a3d1b39e
Lucas Otoni <lotoni.vivo@gmail.com>
2022-05-05 16:14:26 -0300

    pra fixar

---
+++ Front-end/Bloco10-Introdução-a-React/dia-1-Hello-World/my_app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>APP</h1>
+    </div>
+  );
+}
+{/* <App/> */}
+
+export default App;
---
+++ Front-end/Bloco10-Introdução-a-React/dia-1-parte-2/parte2/nome-app/src/Component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Component() {
+  return (
+    <div>
+      <h1>Lucas</h1>
+      <p>Juninho</p>
+    </div>
+  );
+}
