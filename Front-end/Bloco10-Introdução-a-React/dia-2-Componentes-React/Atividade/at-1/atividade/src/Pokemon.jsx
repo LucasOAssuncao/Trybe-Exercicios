c02376659c0f95dbf5714c6bc6d70d87ee55bcc4
Lucas Otoni <lotoni.vivo@gmail.com>
2022-05-09 09:59:25 -0300

    pra fixar 2

--- Front-end/Bloco10-Introdução-a-React/dia-2-Componentes-React/Atividade/at-1/atividade/src/Pokemon.jsx
+++ Front-end/Bloco10-Introdução-a-React/dia-2-Componentes-React/Atividade/at-1/atividade/src/Pokemon.jsx
@@ -4,17 +4,22 @@ export class Pokemon extends React.Component {
   render() {
     const { name, type, averageWeight: peso, image } = this.props.pokemon;
     return (
-      <section className="Pokemon">
+      <section className='Pokemon'>
         <div>
-          <h2>Nome:<br></br> {name}</h2>
-          <h2>Tipo:<br></br> {type}</h2>
+          <h2>
+            Nome:<br></br> {name}
+          </h2>
+          <h2>
+            Tipo:<br></br> {type}
+          </h2>
           <h2>
             Peso:<br></br> {peso.value} {peso.measurementUnit}
           </h2>
         </div>
 
-        <img className="imgPoke" src={image} alt='Imagem de um pokemon' />
+        <img className='imgPoke' src={image} alt='Imagem de um pokemon' />
       </section>
     );
   }
 }
+
