7a0ad321d52d02d5d52c895cdebc0a08ec5ae33c
Lucas Otoni <lotoni.vivo@gmail.com>
2022-08-23 19:56:21 -0300

    feat: atividades 22.3

---
+++ Back-end/Bloco-22-Introdução-Node/22-3-testes-de-integração/cacao-trybe/src/cacaoTrybe.js
@@ -0,0 +1,62 @@
+const fs = require('fs').promises;
+const { join } = require('path');
+
+const readCacaoTrybeFile = async () => {
+  const path = '/files/cacaoTrybeFile.json';
+  try {
+    const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
+    return JSON.parse(contentFile);
+  } catch (error) {
+    return null;
+  }
+};
+
+const updateChocolate = async (id, update) => {
+  const cacaoTrybe = await readCacaoTrybeFile();
+  const chocolateToUpdate = cacaoTrybe.chocolates.find(
+    (chocolate) => chocolate.id === id,
+  );
+
+  if (chocolateToUpdate) {
+    cacaoTrybe.chocolates = cacaoTrybe.chocolates.map((chocolate) => {
+        if (chocolate.id === id) return { ...chocolate, ...update };
+        return chocolate;
+      });
+  
+    await writeCacaoTrybeFile(cacaoTrybe);
+    return { ...chocolateToUpdate, ...update };
+  }
+
+  return false;
+};
+
+const getAllChocolates = async () => {
+  const cacaoTrybe = await readCacaoTrybeFile();
+  return cacaoTrybe.chocolates;
+};
+
+const getChocolateById = async (id) => {
+  const cacaoTrybe = await readCacaoTrybeFile();
+  return cacaoTrybe.chocolates
+    .filter((chocolate) => chocolate.id === id);
+};
+
+const getChocolatesByBrand = async (brandId) => {
+  const cacaoTrybe = await readCacaoTrybeFile();
+  return cacaoTrybe.chocolates
+    .filter((chocolate) => chocolate.brandId === brandId);
+};
+
+const findChocolateByName = async (query) => {
+  const cacaoTrybe = await readCacaoTrybeFile();
+  return cacaoTrybe.chocolates
+    .filter((chocolate) => chocolate.name.toLowerCase().includes(query.toLowerCase()));
+};
+
+module.exports = {
+    getAllChocolates,
+    getChocolateById,
+    getChocolatesByBrand,
+    findChocolateByName,
+    updateChocolate,
+};
