916e10ada4dd8b5a8b343111fe33962d2c9a5b62
Lucas Otoni <lotoni.vivo@gmail.com>
2022-10-05 19:27:58 -0300

    feat: dia 26-3 atividades

--- Back-end/Bloco-26-typescript/26-2/atividade/Atividade5.ts
+++ Back-end/Bloco-26-typescript/26-2/atividade/Atividade5.ts
@@ -3,12 +3,12 @@ type callback<T> = (item: T, index?: number, array?: Array<T>) => boolean;
 function myFilter<T>(array: Array<T>, callback: callback<T>): Array<T> {
     const otherArray: Array<T> = [];
 
-    array.forEach((e) => {
-        if(callback(e)) otherArray.push(e)
+    array.forEach((item) => {
+        if(callback(item)) otherArray.push(item);
     })
 
     return otherArray;
 };
 
 
-console.log(myFilter([1, 2, 3], (item) => item < 3 ));
+console.log(myFilter<number>([1, 2, 3, 4], (item) => item < 3 ));
